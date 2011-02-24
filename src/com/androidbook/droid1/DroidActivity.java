61410d48fa63791404a6ad522eb5fcf0225f18ab
beepscore <support@beepscore.com>
2011-02-23 16:09:43 -0800

    Ref Darcey and Conder Ch 3 Exercise 1 pg 57. In DroidActivity.java, import android.util.Log, add appTag, and log using appTag. Also in AndroidManifest.xml, specify minimum SDK version to eliminate Console warning.

--- src/com/androidbook/droid1/DroidActivity.java
+++ src/com/androidbook/droid1/DroidActivity.java
@@ -2,11 +2,16 @@ package com.androidbook.droid1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DroidActivity extends Activity {
+	
+    private static final String appTag = "Droid1";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	Log.i(appTag, "In onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
