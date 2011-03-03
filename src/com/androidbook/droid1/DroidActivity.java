f4d6209a7c710ba0e24a8fa5a4c9a765f4601c14
beepscore <support@beepscore.com>
2011-03-03 08:19:54 -0800

    Ch 4 finish exercise #4 in DroidActivity.java. In main.xml, reindent.

--- src/com/androidbook/droid1/DroidActivity.java
+++ src/com/androidbook/droid1/DroidActivity.java
@@ -1,6 +1,6 @@
 package com.androidbook.droid1;
 
-import java.io.InputStream;
+import java.io.*;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,13 +13,30 @@ public class DroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	Log.i(appTag, "In onCreate()");
         super.onCreate(savedInstanceState);
+    	Log.i(appTag, "In onCreate() callback method");
         setContentView(R.layout.main);
         
-		InputStream myInputFile = getResources().openRawResource(R.raw.my_text);
-		String myString = myInputFile.toString();
-		Log.v(appTag, myString);
+		InputStream myInputStream = getResources().openRawResource(R.raw.my_text);
+		// create a buffered reader
+		BufferedReader r = new BufferedReader(new InputStreamReader(myInputStream));
+		
+		// use string builder for efficiency
+		StringBuilder total = new StringBuilder();
+		
+		String line = "";
+		try {
+			while ((line = r.readLine()) != null)
+			{ 
+				total.append(line);
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		Log.v(appTag, total.toString());
     }
 
     // override Activity callback methods
@@ -27,38 +44,38 @@ public class DroidActivity extends Activity {
     // http://developer.android.com/reference/android/app/Activity.html
 	@Override
 	protected void onStart() {
-    	Log.i(appTag, "In onStart()");
 		super.onStart();
+    	Log.i(appTag, "In onStart() callback method");
 	}
 	
 	@Override
 	protected void onPause() {
-    	Log.i(appTag, "In onPause()");
 		super.onPause();
+    	Log.i(appTag, "In onPause() callback method");
 	}
 
 	@Override
 	protected void onResume() {
-    	Log.i(appTag, "In onResume()");
 		super.onResume();
+    	Log.i(appTag, "In onResume() callback method");
 	}
 	
 	@Override
 	protected void onStop() {
-    	Log.i(appTag, "In onStop()");
 		super.onStop();
+    	Log.i(appTag, "In onStop() callback method");
 	}
 
 	@Override
 	protected void onRestart() {
-    	Log.i(appTag, "In onRestart()");
 		super.onRestart();
+    	Log.i(appTag, "In onRestart() callback method");
 	}
 	
 	@Override
 	protected void onDestroy() {
-    	Log.i(appTag, "In onDestroy()");
 		super.onDestroy();
+    	Log.i(appTag, "In onDestroy() callback method");
 	}
        
 }
