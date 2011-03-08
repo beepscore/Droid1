6f6d58b4a1ec2a2c4da65b6bcab6fa5b3ec8f80b
beepscore <support@beepscore.com>
2011-03-07 22:00:29 -0800

    Ch 05 Exercise 1.  Add beepscore_icon in sizes 36, 48, and 72 px. Icon image was resized from 57 px rather than vector, and isn't optimum quality. In AndroidManifest.xml, change android:icon from icon to beepscore_icon In simulator, app still showed original icon.png. In simulator, delete app, reload, now it showed new beepscore_icon.png.

--- gen/com/androidbook/droid1/R.java
+++ gen/com/androidbook/droid1/R.java
@@ -18,8 +18,9 @@ public final class R {
         public static final int textDim=0x7f060000;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int sun=0x7f020001;
+        public static final int beepscore_icon=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int sun=0x7f020002;
     }
     public static final class id {
         public static final int imageView1=0x7f080001;
